5ee672dfdb9c57264a218146efc1bc172a30f4ad
shahalam sharif <shahalam.shareef@gmail.com>
2022-07-25 11:02:28 +0600

    server initialized

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+var cors = require("cors");
+const { json } = require("express");
+require("dotenv").config();
+const port = process.env.PORT;
+
+//------ middleware ------------//
+app.use(cors());
+app.use(json());
+
+//------------------------------//
+
+app.get("/", (req, res) => {
+  res.send("Welcome to BookShelf server.");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
